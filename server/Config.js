91b7c89fd045f1f958310934d74de09be8a09153
Raj Mhatre <rajjimh18phpcomp@student.mes.ac.in>
2024-07-11 16:20:48 +0530

    [Added the Middleware]

---
+++ server/Config.js
@@ -0,0 +1,3 @@
+module.exports = {
+       mongoURI: 'mongodb+srv://root:root@airbnb.bybtxc0.mongodb.net/?retryWrites=true&w=majority&appName=AirBnb',
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { mongoURI } = require('./Config')
+const cors = require('cors');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(express.json());
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+// CORS Middleware
+app.use(cors());
+// Own Error Handler
+app.use(errorHandler);
+
+// connection to mongoose
+mongoose.connect(mongoURI)
+       .then(() => {
+              console.log('Mongoose Connected');
+       })
+       .catch((err) => {
+              console.log("error in Connection: " + err.message);
+       });
 
 app.get('/', (req, res) => {
        res.send("Welcome to AirBnb Server");
